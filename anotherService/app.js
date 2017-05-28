ce245696786470315ab78bce3dc0eae1dd581412
GittyGatto <rhenning80@gmx.de>
2017-05-28 11:51:09 +0200

    add person to personList

--- anotherService/app.js
+++ anotherService/app.js
@@ -2,26 +2,28 @@ var app = angular.module('app', []);
 
 app.controller('controllerOne', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService){
 
+    $scope.myService = myService;
+    $scope.myService.personList = personList;
+
     var Person = function(name, age){
         this.age = age;
         this.name = name;
     };
-    
+
     var personList = [];
-    
+
     var bob = new Person('bob', 32);
     var sam = new Person('sam', 42);
-    
+
     personList.push(bob);
     personList.push(sam);
 
-    $scope.myService = myService;
-    $scope.myService.personList = personList;
-    
-    
-    console.log($scope);
-    console.log(myService);
-    console.log(personList);
+    $scope.addPerson = addPerson;
+
+    function addPerson(){
+        newPerson = new Person($scope.name, $scope.age);
+        personList.push(newPerson);
+    }
 }]);
 
 app.controller('controllerTwo', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService){
@@ -33,5 +35,5 @@ app.controller('controllerTwo', ['$rootScope', '$scope', 'myService', function (
 app.service('myService', function(){
 
     this.xxx = "yyyy";
-    
+
 });
