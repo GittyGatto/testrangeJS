4af2aaebba5a1116bc0f045e251d27d867f834aa
GittyGatto <rhenning80@gmx.de>
2017-05-30 22:08:53 +0200

    new button installed and ng-show for inputFields

--- anotherService/app.js
+++ anotherService/app.js
@@ -2,8 +2,6 @@ var app = angular.module('app', []);
 
 app.controller('controllerOne', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService){
 
-    
-
     var Person = function(name, age){
         this.age = age;
         this.name = name;
@@ -14,30 +12,43 @@ app.controller('controllerOne', ['$rootScope', '$scope', 'myService', function (
     var sam = new Person('sam', 42);
     var bill = new Person('bill', 26);
     personList.push(bob, sam, bill);
-    
+
     $scope.myService = myService;
     $scope.myService.personList = personList;
 
+    $scope.inputField = false;
+    $scope.newPerson = newPerson;
     $scope.addPerson = addPerson;
     $scope.delPerson = delPerson;
     $scope.editPerson = editPerson;
 
-    function editPerson(person){
-       
-        $scope.name = person.name;
-        $scope.age = person.age;
-        console.log($scope);
+    function editPerson(person){ 
+        $scope.name = person.person.name;
+        $scope.age = person.person.age;
+        $scope.inputField = true;
     }
-    
+
     function delPerson(person){
         var index = personList.indexOf(person);
         personList.splice(index, 1);
     }
-    
+
     function addPerson(){
         newPerson = new Person($scope.name, $scope.age);
         personList.push(newPerson);
+        $scope.inputField = false;
     }
+
+    function newPerson(){
+        clearInputField();
+        $scope.inputField = true;        
+    }
+
+    function clearInputField(){
+        $scope.name = '';
+        $scope.age = '';
+    }
+
 }]);
 
 app.controller('controllerTwo', ['$rootScope', '$scope', 'myService', function ($rootScope, $scope, myService){
